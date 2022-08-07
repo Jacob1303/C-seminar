9fcb2480ee0aaaebcd10f24e01966965c7fa9e46
yakov kuzin <kuzin_js@mail.ru>
2022-08-07 23:10:58 +0300

    Example work

---
+++ Seminar1/Exp.1/Program.cs
@@ -0,0 +1,44 @@
+﻿// типы данных
+// int, string, bool, float, double 
+// int a = 20; // целочисленные значения
+// string b = "hello, world"; // буквенный тип. Словосочетания, текст, буквы
+// bool c = true or false // значения истина или ложь
+// float d = 5.21; // дроби, например. Не целочисленные значения
+// double e = 5.21; // также не целочисленные значения, но выше объём информации
+
+while (true)
+{
+    Console.WriteLine("Введите оператор +. -. *. / или end для выхода");
+    string Operator = Console.ReadLine();
+
+    if (Operator == "end")
+    {
+        break;
+    }
+
+    Console.WriteLine("Введите первое число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите второе число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+    if (Operator == "+")
+    {
+        Console.WriteLine(number1 + number2);
+    }
+    else if (Operator == "-")
+    {
+        Console.WriteLine(number1 - number2);
+    }
+    else if (Operator == "/")
+    {
+        Console.WriteLine(number1 / number2);
+    }
+    else if (Operator == "*")
+    {
+        Console.WriteLine(number1 * number2);
+    }
+}
+
+
+
